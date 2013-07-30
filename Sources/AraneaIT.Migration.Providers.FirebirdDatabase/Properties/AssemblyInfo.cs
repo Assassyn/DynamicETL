8c810eac5d4daa8ef50217549bc898c5ce7f8ab6
szymon@aranea-it.co.uk <szymon@aranea-it.co.uk>
2013-07-30 21:51:21 +0100

    Added working nuget genration solution

--- Sources/AraneaIT.Migration.Providers.FirebirdDatabase/Properties/AssemblyInfo.cs
+++ Sources/AraneaIT.Migration.Providers.FirebirdDatabase/Properties/AssemblyInfo.cs
@@ -11,13 +11,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AraneaIT.Migration.Providers.FirebirdDatabase")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Providers for the Firebird database")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AraneaIT.Migration.Providers.FirebirdDatabase")]
+[assembly: AssemblyCompany("Aranea IT Ltd")]
+[assembly: AssemblyProduct("Migration Toolkit by Aranea ITs")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTrademark("Aranea IT")]
+[assembly: AssemblyCulture("en-GB")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -37,5 +37,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]

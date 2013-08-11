a427d66098004ae74626a2d458f42073538e81c9
szymon@aranea-it.co.uk <szymon@aranea-it.co.uk>
2013-08-11 22:15:42 +0100

    Added new XSL provider proejct

---
+++ Sources/AraneaIt.Migration.Providers.Excel/BinaryExcelReader.cs
@@ -0,0 +1,27 @@
+﻿namespace AraneaIt.Migration.Providers.Excel
+{
+    using AraneaIT.Migration.Engine;
+    using AraneaIT.Migration.Engine.Actions;
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// Binarty excel  Reader
+    /// </summary>
+    [DebuggerDisplay("Firebird reader:{entityName}")]
+    [Reader("BinaryExcel")]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    internal sealed class BinaryExcelReader : Reader
+    {
+        protected override void AdditionalConfigure(IDictionary<string, string> parametersSet)
+        {
+            base.AdditionalConfigure(parametersSet);
+        }
+
+        public override Entity Read(Entity workingEntity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
